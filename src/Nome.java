d3f4d99d58dcd86740160f0f45baa7481d086905
lowen <lowen.bllaka@escola.pr.gov.br>
2024-09-30 11:37:36 -0300

    iniciando os arquivos

---
+++ src/Nome.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Nome {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+         System.out.println("Digine seu nome:");
+        String nome = leitura.nextLine();
+        System.out.println(nome);
+   }}
